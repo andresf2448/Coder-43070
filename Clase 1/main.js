67f2d6366dd02899c08c887c1ff1b99433fc6a04
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-04-11 18:37:45 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,54 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+// let edad = 15; //forma recomendada para declarar variables
+// var nombre = "pepito"; //forma no recomendable
+// const NUMERO = 3;
+
+// nombrePersonaBonito // Camel Case
+// nombre_persona_bonito // Snake Case
+// PombrePersonaBonito // Pascal Case
+
+// let edad = 56;
+// let nombre = "andres";
+
+// console.log("aca esta la edad", edad);
+// console.log("aca esta el nombre", nombre);
+
+// let numeroUno = 5;
+// let numeroDos = 6;
+// numeroUno = 10;
+// let suma = numeroUno + numeroDos;
+// let resta = numeroUno - numeroDos;
+// let multiplicacion = numeroUno * numeroDos;
+// let division = numeroUno / numeroDos;
+
+// console.log("Aca la suma", suma);
+// console.log("aca la resta", resta);
+// console.log("aca la multiplicacion", multiplicacion);
+// console.log("aca la division", division);
+
+// let saludo = 'Hola';
+// let nombre = "juan";
+// // let saludoCompleto = saludo + " " + nombre;
+// let saludoCompleto = '${saludo} ${nombre} Bienvenido!';
+
+// console.log(saludoCompleto);
+
+// let nombre = parseInt(prompt("Ingrese su nombre"));
+// alert(nombre);
+
+// let numeroUno = parseFloat(prompt("Ingrese el primer numero"));
+// let numeroDos = parseFloat(prompt("Ingrese el segundo numero"));
+
+// alert(numeroUno + numeroDos)
+
+let nombre = "fabian";
+console.log(typeof(nombre));
+
+nombre = 5
