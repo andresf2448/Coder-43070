8a310ad65a23937d2d1ced95a055a5449ad89c90
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-04 18:13:53 -0500

    Clase 8

--- Clase 8/main.js
+++ Clase 8/main.js
@@ -31,11 +31,35 @@
 // let  numero = parseInt(prompt("Ingrese el número"));
 // console.log(aleatorio(numero));
 
-let nombres = ["mariana", "pepito", "juan", "carlos"];
-let cantidad = parseInt(prompt("Ingrese la cantidad de aleatorios que desea"));
+// let nombres = ["mariana", "pepito", "juan", "carlos"];
+// let cantidad = parseInt(prompt("Ingrese la cantidad de aleatorios que desea"));
 
-for(let i = 0; i < cantidad; i++){
-  let indiceAleatorio = Math.round(Math.random() * (nombres.length - 1));
+// for(let i = 0; i < cantidad; i++){
+//   let indiceAleatorio = Math.round(Math.random() * (nombres.length - 1));
 
-  alert(nombres[indiceAleatorio]);
-}
+//   alert(nombres[indiceAleatorio]);
+// }
+
+//DATE
+// console.log(Date());
+// console.log(new Date(2023, 4, 4));
+// const navidad = new Date(2023, 11, 25, 23, 59, 59);
+// console.log(navidad);
+
+// const navidad = new Date("December 25, 2023 23:59:59");
+// console.log(navidad);
+// console.log(navidad.toDateString());
+// console.log(navidad.toLocaleString());
+// console.log(navidad.toLocaleDateString());
+// console.log(navidad.toTimeString());
+// console.log(navidad.getFullYear());
+// console.log(navidad.getMonth());
+// console.log(navidad.getDay());
+
+const navidad = new Date("December 24, 2023");
+const hoy = new Date("May 4, 2023");
+
+console.log(navidad - hoy);
+const milisegundosPorDia = 86400000;
+
+console.log((navidad - hoy) / milisegundosPorDia);
