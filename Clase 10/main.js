07cb930a679d172ef5aa3ad1397b894c074c4fa7
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-11 17:25:05 -0500

    Clase 10

---
+++ Clase 10/main.js
@@ -0,0 +1,21 @@
+let boton = document.getElementById("boton");
+// const respuesta = () => {
+//   console.log("Hola Click");
+// };
+
+//Primera forma
+// boton.addEventListener("click", respuesta);
+
+//segunda forma
+// boton.onclick = respuesta;
+
+// let evento = prompt("Ingrese el evento");
+
+// boton.addEventListener(evento, respuesta);
+
+// const respuesta = (nombre) => {
+//   console.log(`Hola Click ${nombre}`);
+// };
+
+// let nombre = prompt("Ingrese el nombre");
+// boton.addEventListener("click", () => respuesta(nombre));
