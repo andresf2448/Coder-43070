3901fc849854cd80798d2cee8ed9001e2c15b6b0
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-16 17:27:59 -0500

    Clase 11

---
+++ Clase 11/main.js
@@ -0,0 +1,28 @@
+// console.log(localStorage);
+
+// localStorage.setItem("nombre", "andres");
+// localStorage.setItem("numero", 1);
+// localStorage.setItem("valor", "true");
+
+// let nombre = localStorage.getItem("nombre");
+// console.log(nombre);
+
+// let numero = localStorage.getItem("numero");
+// console.log(numero);
+
+// let valor = localStorage.getItem("valor");
+// console.log(valor);
+
+// sessionStorage.setItem("nombre", "andres");
+// sessionStorage.setItem("numero", 1);
+// sessionStorage.setItem("valor", "true");
+
+// for(let i = 0; i < localStorage.length; i++){
+//   let clave = localStorage.key(i);
+//   console.log("clave", clave);
+//   console.log("valor", localStorage.getItem(clave));
+// }
+
+//eliminar datos del storage
+// localStorage.removeItem("nombre"); // para eliminar un dato en especifico
+// localStorage.clear(); // eliminar todo el storage
