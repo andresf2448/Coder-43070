47ef11134b83e8c3296493f82db9f6aa25f57023
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-23 18:16:22 -0500

    Clase 13

--- Clase 13/main.js
+++ Clase 13/main.js
@@ -32,8 +32,71 @@
 
 // console.log(objeto2);
 
-function sumar(...numeros) {
-  return numeros.reduce((acum, item) => acum + item, 0);
-}
+// function sumar(...numeros) {
+//   return numeros.reduce((acum, item) => acum + item, 0);
+// }
 
-console.log(sumar(3, 4, 5, 4, 6, 7, 9, 0, 7, 5, 34, 32));
+// console.log(sumar(3, 4, 5, 4, 6, 7, 9, 0, 7, 5, 34, 32));
+// console.log(sumar(3, 4, 5));
+
+/* 
+if(condicion1){
+  codigo cuando la condicion1 es verdadera
+}else if(condicion2){
+  codigo cuando la condicion2 es verdadera
+}else{
+  codigo a ejecutar cuando ninguna de las condiciones es verdadera
+}
+*/
+
+// let usuario = prompt("Ingrese un usuario");
+
+// while (usuario != "pepito") {
+//   alert("usuario incorrecto");
+//   usuario = prompt("Ingrese un usuario");
+// }
+
+// alert("Bienvenido");
+
+// function sumar(x, y){
+//   return x + y;
+// }
+
+// const sumar = (x, y) => x + y;
+
+// function Persona(nombre, edad) {
+//   this.nombre = nombre;
+//   this.edad = edad;
+//   this.hablar = function () {
+//     console.log(`Hola soy ${this.nombre}`);
+//   };
+// }
+
+// class Persona {
+//   constructor(nombre, edad) {
+//     this.nombre = nombre;
+//     this.edad = edad;
+//   }
+
+//   hablar() {
+//     console.log(`Hola soy ${this.nombre}`);
+//   }
+// }
+
+// const persona1 = new Persona("andres", 45);
+
+//filter
+//forEach
+//map
+//find
+
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputs = e.target.children;
+
+  console.log(inputs[0].value);
+  console.log(inputs[1].value);
+});
