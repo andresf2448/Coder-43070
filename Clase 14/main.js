fc6d7a6423accbe9227af59608a067f126233dc4
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-30 17:34:45 -0500

    Clase 14

---
+++ Clase 14/main.js
@@ -0,0 +1,79 @@
+let boton = document.getElementById("boton");
+
+//SWEETALERT
+// boton.addEventListener("click", () => {
+//   Swal.fire("Good job!", "You clicked the button!", "success");
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Error!",
+//     text: "Error al guardar el producto",
+//     icon: "error",
+//     confirmButtonText: "Cool",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Genial!",
+//     text: "Haz clickeado el botón!",
+//     icon: "success",
+//     confirmButtonText: "Cool",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     position: "top-end",
+//     icon: "success",
+//     title: "Your work has been saved",
+//     showConfirmButton: true,
+//     timer: 1500,
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Sweet!",
+//     text: "Modal with a custom image.",
+//     imageUrl: "https://unsplash.it/400/200",
+//     imageWidth: 400,
+//     imageHeight: 200,
+//     imageAlt: "Custom image",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Está seguro de eliminar el producto?",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonText: "Sí, seguro",
+//     cancelButtonText: "No, no quiero",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       //operaciones codigo
+//       Swal.fire({
+//         title: "Borrado!",
+//         icon: "success",
+//         text: "El archivo ha sido borrado",
+//       });
+//     }
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Ingrese producto",
+//     input: "text",
+//     showCancelButton: true,
+//     confirmButtonText: "Look up",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: `${result.value}`,
+//       });
+//     }
+//   });
+// });
